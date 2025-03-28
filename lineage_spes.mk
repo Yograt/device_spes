a486bcca1f1c311d339147bc6b7d82b0347605da
Yograt <107334627+Yograt@users.noreply.github.com>
2025-03-28 11:51:09 +0700

    Update lineage_spes.mk

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -39,4 +39,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Fingerprint
-BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.7.0.TGCMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys"
