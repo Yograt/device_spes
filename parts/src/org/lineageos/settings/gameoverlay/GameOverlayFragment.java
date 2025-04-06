9899cd8871558362295d236db8d2b9344c31d566
Yograt <fedrix56@gmail.com>
2025-04-06 09:54:34 +0700

    spes: parts: Move to M3 switch for gamespace2.0

--- parts/src/org/lineageos/settings/gameoverlay/GameOverlayFragment.java
+++ parts/src/org/lineageos/settings/gameoverlay/GameOverlayFragment.java
@@ -26,7 +26,7 @@
  import androidx.preference.Preference;
  import androidx.preference.PreferenceFragmentCompat;
  import androidx.preference.SeekBarPreference;
- import androidx.preference.SwitchPreference;
+ import androidx.preference.SwitchPreferenceCompat;
  
  import org.lineageos.settings.R;
  
@@ -34,27 +34,26 @@
  
      private GameOverlay mOverlay;
  
-     private SwitchPreference mMasterSwitch;
+     private SwitchPreferenceCompat mMasterSwitch;
  
-     private SwitchPreference mFpsSwitch;
-     private SwitchPreference mBatteryTempSwitch;
-     private SwitchPreference mCpuUsageSwitch;
-     private SwitchPreference mCpuClockSwitch;
-     private SwitchPreference mCpuTempSwitch;
-     private SwitchPreference mRamSwitch;
+     private SwitchPreferenceCompat mFpsSwitch;
+     private SwitchPreferenceCompat mBatteryTempSwitch;
+     private SwitchPreferenceCompat mCpuUsageSwitch;
+     private SwitchPreferenceCompat mCpuClockSwitch;
+     private SwitchPreferenceCompat mCpuTempSwitch;
+     private SwitchPreferenceCompat mRamSwitch;
  
-     private SwitchPreference mGpuUsageSwitch;
-     private SwitchPreference mGpuClockSwitch;
-     private SwitchPreference mGpuTempSwitch;
+     private SwitchPreferenceCompat mGpuUsageSwitch;
+     private SwitchPreferenceCompat mGpuClockSwitch;
+     private SwitchPreferenceCompat mGpuTempSwitch;
  
      private Preference mCaptureStartPref;
      private Preference mCaptureStopPref;
      private Preference mCaptureExportPref;
  
-     private SwitchPreference mDoubleTapCapturePref;
-     private SwitchPreference mSingleTapTogglePref;
-     private SwitchPreference mLongPressEnablePref;
-     private ListPreference  mLongPressTimeoutPref;
+     private SwitchPreferenceCompat mDoubleTapCapturePref;
+     private SwitchPreferenceCompat mSingleTapTogglePref;
+     private SwitchPreferenceCompat mLongPressEnablePref;
  
      private SeekBarPreference mTextSizePref;
      private SeekBarPreference mBgAlphaPref;
