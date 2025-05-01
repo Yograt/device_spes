f5dd8ddfa736248c5b3a0bc9ae05b3e008d5fcd2
Yograt <107334627+Yograt@users.noreply.github.com>
2025-05-01 18:59:20 +0700

    spes: Switch to NCM USB tethering

--- device.mk
+++ device.mk
@@ -564,6 +564,9 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_PACKAGES += \
+    NcmTetheringOverlay
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
