30ec15ba685822b11c017d73e392b9851e0eba17
Yograt <fedrix56@gmail.com>
2025-05-01 19:28:41 +0700

    Adaptation for Dolby

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/spes/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit Xiaomi Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Product Specifics
 PRODUCT_NAME := lineage_spes
 PRODUCT_DEVICE := spes
