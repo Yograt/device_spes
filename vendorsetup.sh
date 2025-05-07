cc4c6198c71240bafc15786a772c270d4bddc387
Yograt <107334627+Yograt@users.noreply.github.com>
2025-05-07 16:59:19 +0700

    Update vendorsetup.sh
    Change dolby provider

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,8 +9,7 @@ KERNEL_BRANCH="15.0"
 HARDWARE_BRANCH="15.0"
 DEBUG_BRANCH="lineage-22"
 LEICA_CAMERA_BRANCH="leica-5.0"
-DOLBY_DEVICE_BRANCH="15"
-DOLBY_VENDOR_BRANCH="main"
+DOLBY_HARDWARE_BRANCH="15"
 
 check_dir() {
     if [ -d "$1" ]; then
@@ -59,13 +58,9 @@ if check_dir vendor/xiaomi/miuicamera; then
 fi
 
 if check_dir device/xiaomi/dolby; then
-    echo -e "${GREEN}Cloning Dolby sources from dkpost3 (branch: ${YELLOW}$DOLBY_DEVICE_BRANCH${GREEN})...${END}"
-    git clone https://github.com/dkpost3/device_xiaomi_dolby -b $DOLBY_DEVICE_BRANCH device/xiaomi/dolby
+    echo -e "${GREEN}Cloning Dolby sources from dkpost3 (branch: ${YELLOW}$DOLBY_HARDWARE_BRANCH${GREEN})...${END}"
+    git clone https://github.com/Pong-Development/hardware_dolby -b $DOLBY_HARDWARE_BRANCH hardware/dolby
 fi
 
-if check_dir vendor/xiaomi/dolby; then
-    echo -e "${GREEN}Cloning Dolby sources from dkpost3 (branch: ${YELLOW}$DOLBY_VENDOR_BRANCH${GREEN})...${END}"
-    git clone https://github.com/dkpost3/vendor_xiaomi_dolby -b $DOLBY_VENDOR_BRANCH vendor/xiaomi/dolby
-fi
 
 echo -e "${YELLOW}All patches have been successfully applied; your device sources are now ready!${END}"
