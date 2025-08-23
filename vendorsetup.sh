d60078a68b72dd7609fd6bda0ef289303c3a51ed
Yograt <107334627+Yograt@users.noreply.github.com>
2025-08-23 12:23:47 +0700

    spes: Remove qti_kernel_headers

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,5 @@
 # Remove dusplicate decleration
 rm -rf device/lineage/sepolicy/common/vendor/device.te
+
+# Remove conflict qti_kernel_headers by deleting the block's lines
+sed '97,101d' vendor/lineage/build/soong/Android.bp
