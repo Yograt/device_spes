3dfa775bbd2621a1bc7fc67dfa91280cd2e48440
Yograt <107334627+Yograt@users.noreply.github.com>
2025-08-24 10:17:29 +0700

    spes: fix device/xiaomi/spes/lineage_spes.mk:40: error: missing separator.

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -37,7 +37,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Call vendorsetup.sh
-$(shell bash -c "source device/xiaomi/spes/vendorsetup.sh")
+$(shell bash device/xiaomi/spes/vendorsetup.sh)
 
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys"
