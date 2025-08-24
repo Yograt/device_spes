b7cd69fb81a5923413ce86d4467fe1497c1bb7d9
Yograt <107334627+Yograt@users.noreply.github.com>
2025-08-24 10:37:14 +0700

    spes: Moved the vendorsetup.sh line

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -36,8 +36,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
-# Call vendorsetup.sh
-$(shell bash device/xiaomi/spes/vendorsetup.sh)
-
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V816.0.8.0.TGCMIXM:user/release-keys"
