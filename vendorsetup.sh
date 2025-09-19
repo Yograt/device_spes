0cf6e465ced21a1f264192b27e577dca692a643e
Yograt <107334627+Yograt@users.noreply.github.com>
2025-09-19 16:40:02 +0700

    spes: Update commands - BEF

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,6 +11,6 @@ fi
 
 # Remove duplicate qti_kernel_headers block from Android.bp
 if [ -f "$QTI_KERNEL_HEADER_LINEAGE" ]; then
-    echo "Cleaning lines 97–101 from $QTI_KERNEL_HEADER_LINEAGE to avoid build conflict..."
-    sed -i '97,101d' "$QTI_KERNEL_HEADER_LINEAGE"
+    echo "Cleaning lines 107–110 from $QTI_KERNEL_HEADER_LINEAGE to avoid build conflict..."
+    sed -i '107,110d' "$QTI_KERNEL_HEADER_LINEAGE"
 fi
