7d5c0866279bebdf27be35b6a0693fa0a4fda080
Yograt <fedrix56@gmail.com>
2025-11-04 18:34:31 +0000

    Revert "spes: BEF"
    This reverts commit 3fdeed48cc5abe37480f560852cc2f581a1c9345.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Cleanup script to avoid build conflicts
 LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE="device/lineage/sepolicy/common/vendor/device.te"
-DSPVolumeSynchronizer_BOMB="hardware/xiaomi/DSPVolumeSynchronizer/*"
-HARDWARE_XIAOMI_DOLBY_BP="hardware/xiaomi/dolby/Android.bp"
 
 # Remove duplicate SELinux declaration
 if [ -f "$LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE" ]; then
@@ -10,12 +8,3 @@ if [ -f "$LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE" ]; then
     rm -f "$LINEAGE_SEPOLICY_COMMON_VENDOR_DEVICE"
 fi
 
-if [ -f "$DSPVolumeSynchronizer_BOMB" ]; then
-    echo "Deleting $DSPVolumeSynchronizer_BOMB to avoid build conflict..."
-    rm -f "$DSPVolumeSynchronizer_BOMB"
-fi
-
-if [ -f "$HARDWARE_XIAOMI_DOLBY_BP" ]; then
-    echo "Deleting $HARDWARE_XIAOMI_DOLBY_BP to avoid build conflict..."
-    rm -f "$HARDWARE_XIAOMI_DOLBY_BP"
-fi
