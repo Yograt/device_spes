6d1a42d7b7f692885dd8ad75316d1dde5fead44d
Yograt <107334627+Yograt@users.noreply.github.com>
2026-02-10 10:10:44 +0700

    spes: Added Official Flag for EvolutionX

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -26,6 +26,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# EvolutionX Flags
+EVO_BUILD_TYPE := Official
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
