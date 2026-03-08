79ee6d262146b485c8e806047aaad2727687b540
Yograt <fedrix56@gmail.com>
2026-03-08 11:00:56 +0000

    evolution: Update repo and specified branch to sync
    Signed-off-by: Yograt <fedrix56@gmail.com>

--- device.mk
+++ device.mk
@@ -476,4 +476,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 # XiaomiParts
+$(call inherit-product, packages/apps/XiaomiParts/parts.mk)
 include packages/apps/XiaomiParts/parts.mk
+TARGET_USE_CLEARSPEAKER := true
+TARGET_USE_SATURATIONSLIDER := true
+TARGET_USE_DCDIMMING := true
